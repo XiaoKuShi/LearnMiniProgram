2366815c89db3f57bea5f9fac3669fd2c56d50d4
GaoSir_ <472427695@qq.com>
2019-12-15 16:46:16 +0800

    注册小程序和界面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,57 @@
 // pages/home/home.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    userInfo: [],
+    list: [],
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    var that = this
 
+    // 查看是否授权
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              that.setData({
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+  bindGetUserInfo(e) {
+    var that = this
+    that.setData({
+      userInfo: res.userInfo
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  click123123() {
+    console.log("123123被点击了")
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onShow: function() {
+    var that = this
+    var url1 = "http://ios.zmzapi.com/index.php?accesskey=519f9cab85c8059d17544947k361a827&client=1&g=api/v3&m=index&a=article_list&page=0&token=edc3c80aed456c1266237e555b5bf218&uid=5704761"
+    var url2 = "http://ctrl.zmzapi.net/app/ads?platform=7&ver=252"
+    wx.request({
+      url: url1,
+      success (res) {
+        console.log(res.data.data)
+        that.setData({
+          list: res.data.data
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onPageScroll (obj){
+    console.log(obj)
   }
 })
