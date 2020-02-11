337d0b0731f9d8c7d5549963b83a37256112157e
GaoSir_ <472427695@qq.com>
2020-02-11 11:12:37 +0800

    11_弹窗/分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,79 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  onShareAppMessage: function(options) {
+    return {
+      title: '你好啊，小乐乐',
+      path: '/pages/home/home',
+      imageUrl: '/asserts/002.jpg'
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowToast1() {
+    wx.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 3000
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowToast2() {
+    wx.showToast({
+      title: '加载中...',
+      icon: 'loading',
+      duration: 3000
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowToast3() {
+    wx.showToast({
+      title: '今天是个好日子，天气好极了',
+      icon: 'none',
+      duration: 3000
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleShowModal1() {
+    wx.showModal({
+      title: '提示',
+      content: '是否要退出登录',
+      showCancel: true,
+      cancelText: '取消吧',
+      cancelColor: '#222222',
+      confirmText: '就退咋地',
+      confirmColor: '#ff0000',
+      success: function(res) {
+        console.log('modal弹出成功')
+        if(res.cancel) {
+          console.log('点击了取消')
+        }
+        if (res.confirm) {
+          console.log('点击了确定')
+        }
+      },
+      fail: function() {
+        console.log('modal弹出失败')
+      },
+      complete: function() {
+        console.log('不知道modal有没有弹出')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  handleShowLoading1() {
+    wx.showLoading({
+      title: '等待中...',
+      mask: true
+    })
+    //需要手动隐藏
+    setTimeout(()=> {
+      wx.hideLoading()
+    }, 2000)
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleShowAction1() {
+    wx.showActionSheet({
+      itemList: ['1','2','3'],
+      success(res) {
+        console.log(res.tapIndex)
+      },
+      fail(res) {
+        console.log(res.errMsg)
+      }
+    })
   }
 })
